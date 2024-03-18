862d71207b7a9c5a944e45102ea618e8fbb8b491
Martina Kraus <kraus.martina.m@gmail.com>
2024-03-18 16:08:13 +0100

    Remove Solution

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withXsrfConfiguration } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withXsrfConfiguration({})),
+    provideHttpClient(),
     provideRouter(routes, withComponentInputBinding())
   ]
 };
